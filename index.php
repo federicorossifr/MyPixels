62d17b09f0c6539fb9f6823127886edf7d107e55
Federico Rossi <Federico Rossi>
2016-07-17 11:31:25 +0400

    Started UI/UX developement

--- index.php
+++ index.php
@@ -2,10 +2,21 @@
 <html>
 <head>
 	<title>Pxls</title>
+	<link rel="stylesheet" type="text/css" href="./css/font.css">
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<link rel="stylesheet" type="text/css" href="./css/topbar.css">
 </head>
 <body>
-	<a href="./testPicturesFunctionalities.php">Pic</a>
-	<a href="./testUserFunctionalities.php">User</a>
-	<a href="./tagTest.html">TagTest</a>
+	<nav id="topBar">
+		<a href="" class="brand">Pixels</a>
+		<ul class="links">
+			<li><a href=""><img src="./res/home.png"><img src="./res/homeDark.png"></a></li>
+			<li><a href=""><img src="./res/heart.png"><img src="./res/heartDark.png"></a></li>
+			<li><a href=""><img src="./res/explore.png"><img src="./res/exploreDark.png"></a></li>
+			<li><a href=""><img src="./res/profile.png"><img src="./res/profileDark.png"></a></li>
+		</ul>
+	</nav>
+
+
 </body>
 </html>
